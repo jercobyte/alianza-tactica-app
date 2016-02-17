a3ce6a09099b0147c998f6414081cde4a4579017
jercobyte <jercobyte@gmail.com>
2016-02-17 19:59:05 +0100

    Added social links to members, club description and little style changes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,10 @@
 members = [
-    { nombre: 'Dani Pérez', id: 0},
-    { nombre: 'Jonathan García', id: 1 },
-    { nombre: 'Juan Manuel Pinto', id: 2 },
-    { nombre: 'José Antonio Gómez', id: 3 },
-    { nombre: 'Piter Arjona', id: 4 },
-    { nombre: 'Ricardo León', id: 5 },
+    { nombre: 'Dani Pérez', imagen: 'img/miembros/Dani.jpg', facebook: 'https://www.facebook.com/dani.perezesteban', twitter: 'https://twitter.com/daj26kc', id: 0},
+    { nombre: 'Jonathan García', imagen: 'img/miembros/Jonathan.jpg', facebook: 'https://www.facebook.com/jonathan.garciavillodres', twitter: 'https://twitter.com/JonathanGV23', instagram: 'https://www.instagram.com/jonathangv26/', id: 1 },
+    { nombre: 'Juan Manuel Pinto', imagen: 'img/miembros/Juanma.jpg', facebook: 'https://www.facebook.com/juanmanuel.pintorodriguez', instagram: 'https://www.instagram.com/juanmanuelpintorodriguez/', id: 2 },
+    { nombre: 'José Antonio Gómez', imagen: 'img/miembros/Jose.jpg', facebook: 'https://www.facebook.com/joseantonio.gomezlopez', twitter: 'https://twitter.com/JoseAGM_90', instagram: 'https://www.instagram.com/joseagm90/', id: 3 },
+    { nombre: 'Piter Arjona', imagen: 'img/miembros/Pedro.jpg', facebook: 'https://www.facebook.com/pedrojesus.arjonasiles', twitter: 'https://twitter.com/piterarjonasile', instagram: 'https://www.instagram.com/piter_algaidas', id: 4 },
+    { nombre: 'Ricardo León', imagen: 'img/miembros/Ricardo.jpg', facebook: 'https://www.facebook.com/ricardoluis.leon', id: 5 },
   ];
 
 angular.module('starter.controllers', [])
